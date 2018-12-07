ecff4c8edf2565ce1743b84077d290ccd1109978
Hector Jose Taveras Rodriguez <hectojo_2109@hotmail.com>
2018-12-07 19:05:27 -0400

    qerert

---
+++ PRESENTACION/FormAdministrarMedic.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PRESENTACION
+{
+    public partial class FormAdministrarMedic : Form
+    {
+        conccionsql oper = new conccionsql();
+        public FormAdministrarMedic()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            oper.consultasinreaultado("");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
